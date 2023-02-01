1f676d748db56116398cc1980719849d7cac3eb0
Isaac <112578560+IsaacWW3@users.noreply.github.com>
2023-02-01 11:34:22 -0500

    Add files via upload

---
+++ class_ex1.py
@@ -0,0 +1,4 @@
+num = float(input("Number of hours worked: "))
+num2 = float(input("Amount paid per hour: "))
+pay = num * num2
+print("The pay is $", float(pay))
---
+++ worcester_lab1.py
@@ -0,0 +1,55 @@
+'''
+TASK 1
+print("any sentence")
+
+lname = input("Enter your last name: ")
+
+id = int(input("Enter your student ID: "))
+
+print("Your student ID is,", id, "Your last name is,", lname)
+
+# TASK 2
+# QUESTION 1
+#  a. the data type is an int
+#  b. the data type is a string
+#  c. the data type is a string
+#  d. the data type is a float
+# QUESTION 2
+#  A. int B. string C. int D. int E. int F. int
+
+# TASK 3
+# 1. 0.5 2. 0.5 3. 0.5 4. 0.25 5. 110 6. 1 7. 0 8. 12.45
+
+x = 1/2
+x = 1 / 2.0
+x = 1 // 2
+x = 1 / 2 / 2
+x = 100 * 1.1
+x = 3 % 2
+x = 6 % 3
+x = 25 ** 0.5
+
+print(x)
+
+TASK 4
+data1 = 99.9 int
+data2 = int(data1) # int
+data3 = str(data2) # string
+data4 = bool(data3) # boolean
+data5 = data1 - int(data1) # int
+data6 = data4 + data1 # boolean???
+
+TASK 5
+5 == 4 + 1 # True
+3 != 3.0 # False
+1/2 > 1//2 # True
+True == int(False) + 1 # True
+(5 == 4) + 1 # False
+(5 == 4 + 1) + 1 #True
+0 or 3 # True
+-1 or 1 # True
+3 and 0 # True
+1 and not 2 # True
+'''
+
+
